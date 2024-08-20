631dd7920b00f114c41583bdd88ab4a6d4101fe8
Souley <souleynu@gmail.com>
2024-08-20 18:37:36 -0400

    pantalla de inicio sin interacciones

--- proyectoHM/App.js
+++ proyectoHM/App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'; //barra superior de hora y bateria, se utiliza con estilo automatico (black/white)
+import React from 'react'; //por default hay que tenerlo
+import { StyleSheet, Text, View, TextInput } from 'react-native'; //nos permite crear la función y guardarla en la variable styles
+
+//div = view
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>Bienvenido</Text>
+      <Text style={styles.subtitulo}>Ingresa tu correo</Text>
+      <TextInput 
+        placeholder='usuario@gmail.com'
+        style={styles.texto_inputs}
+      />
+      <TextInput 
+        placeholder='contraseña'
+        style={styles.texto_inputs}
+        secureTextEntry={true}
+      />
+      <Text style={styles.olvidaste_contraseña}>Olvidaste tu contraseña</Text>
+      <Text style={styles.olvidaste_contraseña}>¿No tienes una cuenta?</Text>
+      <StatusBar style="auto" /> 
     </View>
   );
 }
@@ -13,8 +28,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 55,
+    color: '#000',
+    fontWeight: 'bold',
+    fontFamily: 'System' 
+  },
+  subtitulo: {
+    fontSize: 20,
+    color: 'gray',
+    fontFamily: 'System'
+  },
+  texto_inputs: {
+    //borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    paddingStart: 20,
+    width: '70%',
+    marginTop: 20,
+    textAlign: 'center',
+    borderRadius: 30,
+    height: 50,
+    backgroundColor: '#fff',
+  },
+  olvidaste_contraseña: {
+    fontSize: 14,
+    fontFamily: 'System',
+    color: 'gray',
+    marginTop: 20,
+  }
+
+
+
 });
