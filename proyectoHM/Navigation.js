640e4711c6b52187b53d014304f7b6dcd148e28c
Souley <souleynu@gmail.com>
2024-09-15 13:51:57 -0300

    cambio de iconos en tab

--- proyectoHM/Navigation.js
+++ proyectoHM/Navigation.js
@@ -2,6 +2,10 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 
+//iconos
+import AntDesign from '@expo/vector-icons/AntDesign';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 //ventanas
 import Home from "./ventanas/Home";
 import Settings from "./ventanas/Settings";
@@ -11,8 +15,8 @@ const Tab = createBottomTabNavigator();
 export default function MyTabs() {
     return (
         <Tab.Navigator>
-            <Tab.Screen  name="Home" component={Home} />
-            <Tab.Screen  name="Settings" component={Settings} />
+            <Tab.Screen  name="Home" component={Home} options={{ tabBarLabel: 'Inicio', tabBarIcon: () => (<AntDesign name="home" size={24} color="black" />) }} />
+            <Tab.Screen  name="Settings" component={Settings} options={{ tabBarLabel: 'Configuración', tabBarIcon: () => (<MaterialCommunityIcons name="account-edit-outline" size={24} color="black" />) }}/>
         </Tab.Navigator>
     );
 }
